6c8d41e115416e9f428c7bd080c975a6c7284616
Nour Abdi <34486030+NourAbdi@users.noreply.github.com>
2024-08-07 18:49:19 +0300

    sad

--- src/models/ShopCategory.js
+++ src/models/ShopCategory.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 // Define the schema
 const categorySchema = new mongoose.Schema({
   categoryName: { type: String, required: true },
-  categoryPhoto: { type: String, required: true },
+  categoryPhoto: { type: String, required: false },
 });
 
 // Create the model
