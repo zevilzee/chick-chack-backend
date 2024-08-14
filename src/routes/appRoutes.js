ab3809e8446cfe6b09a3b9092d8872646e8f2922
Nour Abdi <34486030+NourAbdi@users.noreply.github.com>
2024-08-14 11:00:16 +0300

    finished new model server

--- src/routes/appRoutes.js
+++ src/routes/appRoutes.js
@@ -3,12 +3,14 @@ import {
   createAppInfo,
   getAppInfo,
   updateAppInfo,
+  deleteAppInfo // Import the delete function
 } from "../controllers/AppController.js";
 
 const router = express.Router();
 
 router.post("/", createAppInfo);
 router.get("/", getAppInfo);
-router.put("/:id", updateAppInfo); // Added `:id` for update endpoint
+router.put("/:id", updateAppInfo);
+router.delete("/:id", deleteAppInfo); // Add DELETE route
 
 export default router;
